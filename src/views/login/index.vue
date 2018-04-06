b3013e9b2378aae72dee2f080560d335c2ef780a
QM36 <18791767622@163.com>
2018-04-06 23:50:03 +0800

    修改注册页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,9 +16,9 @@
         <Form-item>
             <Button type="primary" @click="handleLogin('loginForm')" long>登录</Button>
         </Form-item>
-        <div class='tips'>admin账号为:admin@wz.com 密码123456</div>
-            <div class='tips'>editor账号:editor@wz.com 密码123456</div>
-           </Form>
+            <!-- <div class='tips'>admin账号为:admin@wz.com 密码123456</div>
+            <div class='tips'>editor账号:editor@wz.com 密码123456</div> -->
+    </Form>
 
     </div>
 </template>
